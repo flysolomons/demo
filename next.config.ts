e87c07406259bfc82f2794b9fec2eee118245d54
Joshua Zobule <joshwhizkid@gmail.com>
2025-02-17 15:58:58 +1100

    config update

--- next.config.ts
+++ next.config.ts
@@ -10,11 +10,7 @@ const nextConfig = {
   eslint: {
     // Temporarily ignore ESLint errors during build
     ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Temporarily ignore TypeScript errors during build
-    ignoreBuildErrors: true,
-  },
+  }
 }
 
 module.exports = nextConfig
